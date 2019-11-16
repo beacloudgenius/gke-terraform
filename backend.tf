f915e6250e265c525b62c9261bd782aa6144b231
Nilesh Londhe <nilesh@cloudgeni.us>
2019-11-15 20:37:22 -0800

    s3 protected backend

--- backend.tf
+++ backend.tf
@@ -8,12 +8,14 @@ provider "aws" {
 terraform {
   backend "s3" {
     key            = "global/s3/terraform.tfstate"
+
+    # Replace this with your region name
     region         = "us-west-2"
 
-    # Replace this with your bucket name!
+    # Replace this with your bucket name
     bucket         = "cgterraform"
 
-    # Replace this with your DynamoDB table name!
+    # Replace this with your DynamoDB table name
     dynamodb_table = "cgterraform"
 
     encrypt        = true
